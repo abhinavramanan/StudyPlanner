5f4c45ac4b85b8a384f362b720ad77dbd4765d58
Abhinav Ramanan <hi@abhinavramanan.me>
2025-08-12 17:35:54 +0530

    Fix separation error

--- StudyPlanner/settings.py
+++ StudyPlanner/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1', "studyplanner-hj4y.onrender.com").split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost,127.0.0.1,studyplanner-hj4y.onrender.com').split(',')
+
 
 
 # Application definition
